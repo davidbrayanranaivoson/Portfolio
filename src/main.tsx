4fbb609fd8e5dab6b8a82e631758a95810fae54f
David Brayan <davidbrayanranaivoson@gmail.com>
2026-09-16 07:34:42 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <BrowserRouter basename="/Portfolio">
+  <App />
+</BrowserRouter>
 );
