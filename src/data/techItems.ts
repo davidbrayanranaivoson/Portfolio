455db870041c67fcdc77ce3763467e6d3e21d9e8
David Brayan <davidbrayanranaivoson@gmail.com>
2026-09-16 07:39:50 +0300

    Update techItems.ts

--- src/data/techItems.ts
+++ src/data/techItems.ts
@@ -36,7 +36,7 @@ export const techItems = [
     nom: "Tailwind Css",
     link: "logo-tailwind-css.png",
     niveau: "90%",
-    desciption: "Connaissance des bases de Tailwind, mise en palce de thème.",
+    desciption: "Connaissance des bases de Tailwind, mise en place de thème.",
   },
   {
     id: 6,
